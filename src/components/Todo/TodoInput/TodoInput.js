a062adac66cd5d6468a219da671d25e14aee64fe
Konstantinos Kalkounos <kostiskalkounos@gmail.com>
2021-05-09 21:24:26 +0200

    add dynamic styled-components

--- src/components/Todo/TodoInput/TodoInput.js
+++ src/components/Todo/TodoInput/TodoInput.js
@@ -1,7 +1,42 @@
 import { useState } from "react";
+import styled from "styled-components";
 
 import Button from "../../UI/Button/Button";
 import "./TodoInput.css";
+
+const FormControl = styled.div`
+  margin: 0.5rem 0;
+
+  & label {
+    font-weight: bold;
+    display: block;
+    margin-bottom: 0.5rem;
+  }
+
+  & input {
+    display: block;
+    width: 100%;
+    border: 1px solid #ccc;
+    font: inherit;
+    line-height: 1.5rem;
+    padding: 0 0.25rem;
+  }
+
+  & input:focus {
+    outline: none;
+    background: #fad0ec;
+    border-color: #8b005d;
+  }
+
+  &.invalid input {
+    border-color: red;
+    background: pink;
+  }
+
+  &.invalid label {
+    color: red;
+  }
+`;
 
 const TodoInput = (props) => {
   const [enteredValue, setEnteredValue] = useState("");
@@ -25,10 +60,10 @@ const TodoInput = (props) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className={`form-control ${!isValid ? "invalid" : ""}`}>
+      <FormControl className={!isValid && "invalid"}>
         <label>What are your plans for today?</label>
         <input type="text" onChange={todoInputChangeHandler} />
-      </div>
+      </FormControl>
       <Button type="submit">Add Item</Button>
     </form>
   );
